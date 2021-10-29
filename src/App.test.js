059503b276f5a0d4798a172d3838fd39ba830c25
Lilmcg <lilmcg@gmail.com>
2021-10-29 09:51:29 -0700

    Enzyme test coverage complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 import App from './App';
+import Home from './pages/Home'
+import DogIndex from './pages/DogIndex'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('When the app renders', () => {
+  it('displays a header and footer', () => {
+
+    const renderedApp = shallow(<App/>)
+    const renderedHeader = renderedApp.find("Header")
+    const renderedFooter = renderedApp.find("Footer")
+
+    expect(renderedHeader.length).toEqual(1)
+    expect(renderedFooter.length).toEqual(1)
+  })
+
+  it('provides a route "/" to the home component', () => {
+
+    const renderedApp = shallow(<App/>)
+    const renderedHomeRoute = renderedApp.find('[path="/"]')
+
+    expect(renderedHomeRoute.props().component).toEqual(Home)
+  })
+
+  it('provides a route "/dogindex" to the DogIndex component', () => {
+
+    const renderedApp = shallow(<App/>)
+    const renderedDogIndexRoute = renderedApp.find('[path="/dogindex"]')
+
+    expect(renderedDogIndexRoute.props().component).toEqual(DogIndex)
+  })
+})
