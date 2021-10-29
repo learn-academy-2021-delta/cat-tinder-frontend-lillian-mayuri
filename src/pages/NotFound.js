a6959d46868fc1b749165877576b63d2eca5e96c
Lilmcg <lilmcg@gmail.com>
2021-10-29 10:24:54 -0700

    fixed test coverage files

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 class NotFound extends Component {
   render() {
     return (
-    <>
-    <h3>I am not Not Found</h3>
-    </>
-  )
+      <div className="page-body">
+        <h3>Sorry no dogs here!</h3>
+        <ul>
+        <NavLink to="/">Return to Home Page</NavLink>
+      </ul>
+      </div>
+    )
   }
 }
 export default NotFound
