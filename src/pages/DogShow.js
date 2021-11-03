a51fde21fddd2f2b843f5dcd4cef6d8b0211652f
Mayuridalavai <mayuri.gj@gmail.com>
2021-11-03 11:12:39 -0700

    styling and changes to show

--- src/pages/DogShow.js
+++ src/pages/DogShow.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { Button, Card, CardTitle, CardText } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 
 class DogShow extends Component {
-  render() {
-    return (
-      <>
-        <h1>I am the Dog Show</h1>
-      </>
-    )
-  }
+ render() {
+   let {dog} = this.props
+   return (
+     <div className="page-body">
+       {dog &&
+        <Card body className="card-show">
+          <CardTitle tag="h5">Hi! My name is {dog.name}</CardTitle>
+          <h2>🐕</h2>
+          <CardText>I am {dog.age} years old</CardText>
+          <CardText>I enjoy {dog.enjoys}</CardText>
+          <NavLink to={`/dogedit/${dog.id}`}>
+            <Button>Edit Dog Profile</Button>
+          </NavLink>
+          <br />
+          <NavLink to="/dogindex">
+            <Button onClick={() => this.props.deleteDog(dog.id)}>
+              Delete Dog Profile
+            </Button>
+          </NavLink>
+        </Card>
+       }
+     </div>
+   )
+ }
 }
-
 export default DogShow
